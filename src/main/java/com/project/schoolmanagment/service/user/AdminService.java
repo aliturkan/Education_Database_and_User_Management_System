48d7b87b65a98ffbd11edbd99b7fa3ac73bbb5b8
aliturkan <aliturkan@gmail.com>
2023-08-23 13:26:13 -0500

    day2

--- src/main/java/com/project/schoolmanagment/service/user/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.project.schoolmanagment.service.user;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class AdminService {
-}
