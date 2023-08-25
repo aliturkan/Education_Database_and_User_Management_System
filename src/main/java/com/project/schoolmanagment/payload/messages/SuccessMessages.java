28266a2de126600259c6027588c47cb72988385d
aliturkan <aliturkan@gmail.com>
2023-08-25 03:26:57 -0500

    day3

--- src/main/java/com/project/schoolmanagment/payload/messages/SuccessMessages.java
+++
@@ -1,7 +0,0 @@
-package com.project.schoolmanagment.payload.messages;
-
-public abstract class SuccessMessages {
-
-	//Admin
-	public static final String ADMIN_CREATE = "Admin created successfully";
-}
