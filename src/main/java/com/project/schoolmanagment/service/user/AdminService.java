15a337145698ad9f07b71584c8c91e41c5daaec2
aliturkan <aliturkan@gmail.com>
2023-08-25 03:20:39 -0500

    day2

---
+++ src/main/java/com/project/schoolmanagment/service/user/AdminService.java
@@ -0,0 +1,105 @@
+package com.project.schoolmanagment.service.user;
+
+import com.project.schoolmanagment.entity.concretes.user.Admin;
+import com.project.schoolmanagment.entity.enums.RoleType;
+import com.project.schoolmanagment.exception.ConflictException;
+import com.project.schoolmanagment.exception.ResourceNotFoundException;
+import com.project.schoolmanagment.payload.mappers.AdminMapper;
+import com.project.schoolmanagment.payload.messages.ErrorMessages;
+import com.project.schoolmanagment.payload.messages.SuccessMessages;
+import com.project.schoolmanagment.payload.request.user.AdminRequest;
+import com.project.schoolmanagment.payload.response.message.ResponseMessage;
+import com.project.schoolmanagment.payload.response.user.AdminResponse;
+import com.project.schoolmanagment.payload.response.user.DeanResponse;
+import com.project.schoolmanagment.repository.user.AdminRepository;
+import com.project.schoolmanagment.service.helper.PageableHelper;
+import com.project.schoolmanagment.service.validator.UniquePropertyValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+	private final AdminMapper adminMapper;
+	private final AdminRepository adminRepository;
+	private final UserRoleService userRoleService;
+	private final UniquePropertyValidator uniquePropertyValidator;
+	private final PageableHelper pageableHelper;
+
+	public ResponseMessage<AdminResponse>saveAdmin(AdminRequest adminRequest){
+
+		//we need to check duplication
+		uniquePropertyValidator.checkDuplicate(adminRequest.getUsername(),
+												adminRequest.getSsn(),
+												adminRequest.getPhoneNumber());
+
+		// we must map DTO -> Entity
+		Admin admin = adminMapper.mapAdminRequestToAdmin(adminRequest);
+
+		//we are setting role type from roles table
+		admin.setUserRole(userRoleService.getUserRole(RoleType.ADMIN));
+
+		//superAdmin will not be deleted or changed
+		if(Objects.equals(adminRequest.getUsername(),"superAdmin")){
+			admin.setBuiltIn(true);
+		}
+
+		Admin savedAdmin= adminRepository.save(admin);
+
+		// we are returning response DTO by mapping the saved version of admin.
+		return ResponseMessage.<AdminResponse>builder()
+				.message(SuccessMessages.ADMIN_CREATE)
+				.object(adminMapper.mapAdminToAdminResponse(savedAdmin))
+				.build();
+
+	}
+
+	public String deleteById(Long id){
+		//we should check the database if this id really exist
+		Optional<Admin>admin =adminRepository.findById(id);
+		if(admin.isEmpty()){
+			throw new ResourceNotFoundException(String.format(ErrorMessages.NOT_FOUND_USER_MESSAGE,id));
+		} else if (admin.get().isBuiltIn()) {
+			throw new ConflictException(ErrorMessages.NOT_PERMITTED_METHOD_MESSAGE);
+		}
+		adminRepository.deleteById(id);
+		return SuccessMessages.ADMIN_DELETE;
+	}
+
+	public Page<Admin> getAllAdminsByPage(int page, int size, String sort, String type){
+		Pageable pageable = pageableHelper.getPageableWithProperties(page, size, sort, type);
+		return adminRepository.findAll(pageable);
+	}
+
+// 1.way
+//	public List<AdminResponse> getAllAdmins() {
+//		List<Admin> adminList = adminRepository.findAll();
+//		List<AdminResponse> adminResponses = new ArrayList<>();
+//
+//		for (Admin admin : adminList) {
+//			AdminResponse adminResponse = adminMapper.mapAdminToAdminResponse(admin);
+//			adminResponses.add(adminResponse);
+//		}
+//		return adminResponses;
+//	}
+// 2.way with Lambda
+	public List<AdminResponse> getAllAdmins(){
+		return adminRepository.findAll()
+				.stream()
+				.map(adminMapper::mapAdminToAdminResponse)
+				.collect(Collectors.toList());
+	}
+
+	public long countAllAdmins(){
+		return adminRepository.count();
+	}
+}
